edbe0b56d1e03e022517412714334ce54856ce98
fizzday <fizzday@yeah.net>
2019-10-18 16:11:09 +0800

    fix

--- error.go
+++ error.go
@@ -2,12 +2,14 @@ package e
 
 import (
 	"errors"
+	"fmt"
 	"runtime"
 )
 
 type E interface {
 	error
 	ToError() error
+	Stack() Stack
 }
 type Stack struct {
 	File     string
@@ -15,17 +17,33 @@ type Stack struct {
 	FuncName string
 }
 type Error struct {
-	Msg   string
-	Stack Stack
+	msg   string
+	stack Stack
 }
 
-func New(arg string) Error {
+func New(msg string) Error {
 	var err = Error{
-		Msg: arg,
+		msg: msg,
 	}
 	funcName, file, line, ok := runtime.Caller(1)
 	if (ok) {
-		err.Stack = Stack{
+		err.stack = Stack{
+			File:     file,
+			Line:     line,
+			FuncName: runtime.FuncForPC(funcName).Name(),
+		}
+	}
+
+	return err
+}
+
+func NewWithError(msg string, goErr error) Error {
+	var err = Error{
+		msg: fmt.Sprint(msg, ":", goErr.Error()),
+	}
+	funcName, file, line, ok := runtime.Caller(1)
+	if (ok) {
+		err.stack = Stack{
 			File:     file,
 			Line:     line,
 			FuncName: runtime.FuncForPC(funcName).Name(),
@@ -36,9 +54,13 @@ func New(arg string) Error {
 }
 
 func (e Error) ToError() error {
-	return errors.New(e.Msg)
+	return errors.New(e.msg)
 }
 
 func (e Error) Error() string {
-	return e.Msg
+	return e.msg
+}
+
+func (e Error) Stack() Stack {
+	return e.stack
 }
--- error_test.go
+++ error_test.go
@@ -1,6 +1,7 @@
 package e
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -13,6 +14,23 @@ func TestNew(t *testing.T) {
 	t.Log("line:",err.Stack.Line)
 	t.Log("func name:",err.Stack.FuncName)
 	fmt.Printf("%#v",err)
+}
+
+func TestNewWithError(t *testing.T) {
+	goErr := errors.New("这是官方标准错误")
+	err := NewWithError("手动添加的错误", goErr)
+
+	t.Log("msg:",err.Msg)
+	t.Log("file:",err.Stack.File)
+	t.Log("line:",err.Stack.Line)
+	t.Log("func name:",err.Stack.FuncName)
+	fmt.Printf("%#v",err)
+}
+
+func TestError_Error(t *testing.T) {
+	err := New("only show a custom errors demo")
+
+	t.Log(err.Error())
 }
 
 
