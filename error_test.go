923440c7cea74c4c7f4e0669f06d54ae58a5343f
fizzday <fizzday@yeah.net>
2019-10-18 16:13:14 +0800

    fix

--- error_test.go
+++ error_test.go
@@ -9,10 +9,10 @@ import (
 func TestNew(t *testing.T) {
 	err := New("only show a custom errors demo")
 
-	t.Log("msg:",err.Msg)
-	t.Log("file:",err.Stack.File)
-	t.Log("line:",err.Stack.Line)
-	t.Log("func name:",err.Stack.FuncName)
+	t.Log("msg:",err.Error())
+	t.Log("file:",err.Stack().File)
+	t.Log("line:",err.Stack().Line)
+	t.Log("func name:",err.Stack().FuncName)
 	fmt.Printf("%#v",err)
 }
 
@@ -20,10 +20,10 @@ func TestNewWithError(t *testing.T) {
 	goErr := errors.New("这是官方标准错误")
 	err := NewWithError("手动添加的错误", goErr)
 
-	t.Log("msg:",err.Msg)
-	t.Log("file:",err.Stack.File)
-	t.Log("line:",err.Stack.Line)
-	t.Log("func name:",err.Stack.FuncName)
+	t.Log("msg:",err.Error())
+	t.Log("file:",err.Stack().File)
+	t.Log("line:",err.Stack().Line)
+	t.Log("func name:",err.Stack().FuncName)
 	fmt.Printf("%#v",err)
 }
 
