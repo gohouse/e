e7a90acf22d68c2af12b5000a8e4742f862d3d7d
fizzday <fizzday@yeah.net>
2019-12-07 03:02:41 +0000

    log cors

--- logger.go
+++ logger.go
@@ -5,16 +5,19 @@ import (
 	"os"
 )
 
-func Log() HandlerFunc {
+func Log(fileNames ...string) HandlerFunc {
 	return func(ctx *ErrorContext) {
 		var fileName = "./error-statck.log"
+		if len(fileNames) > 0 {
+			fileName = fileNames[0]
+		}
 		f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		if err != nil {
 			fmt.Println(err)
 		}
 		//write to the file
-		_, err = fmt.Fprint(f, "error_msg:", ctx.ErrorWithStack(),"--------------------------------------------------\n")
-		if err!=nil {
+		_, err = fmt.Fprint(f, "error_msg:", ctx.ErrorWithStack(), "--------------------------------------------------\n")
+		if err != nil {
 			panic(err.Error())
 		}
 		f.Close()
